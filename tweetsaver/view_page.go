7886ed3fe6c4f99b6a621e5a481f2a6efc7956f6
Casey Artner <Casey@Artner.co>
2014-03-24 07:43:19 -0700

    use blueprint

--- tweetsaver/view_page.go
+++ tweetsaver/view_page.go
@@ -1,16 +1,21 @@
 package tweetsaver
 
 import (
-	"html/template"
+	"github.com/ArtnerC/blueprint"
+	//"html/template"
 	"net/http"
 )
 
-var TemplateDir = "../templates/"
+func init() {
+	blueprint.MustCompileDir("Master.html", "../templates/")
+}
 
-var (
-	ItemTemplate     = template.Must(template.ParseFiles(usingMaster(TemplateDir, "Item.html")...))
-	ItemListTemplate = template.Must(template.ParseFiles(usingMaster(TemplateDir, "ItemList.html")...))
-)
+// var TemplateDir = "../templates/"
+
+// var (
+// 	ItemTemplate     = template.Must(template.ParseFiles(usingMaster(TemplateDir, "Item.html")...))
+// 	ItemListTemplate = template.Must(template.ParseFiles(usingMaster(TemplateDir, "ItemList.html")...))
+// )
 
 func usingMaster(base, name string) []string {
 	return []string{base + "Master.html", base + name}
@@ -25,15 +30,23 @@ func NewPageView(w http.ResponseWriter) *PageView {
 }
 
 func (pv *PageView) DisplayItem(t *Tweet) {
-	if err := ItemTemplate.Execute(pv.response, t); err != nil {
+	if err := blueprint.Execute(pv.response, "Item.html", t); err != nil {
 		http.Error(pv.response, err.Error(), http.StatusInternalServerError)
 	}
+
+	// if err := ItemTemplate.Execute(pv.response, t); err != nil {
+	// 	http.Error(pv.response, err.Error(), http.StatusInternalServerError)
+	// }
 }
 
 func (pv *PageView) DisplayAll(tweets []*Tweet) {
-	if err := ItemListTemplate.Execute(pv.response, tweets); err != nil {
+	if err := blueprint.Execute(pv.response, "ItemList.html", tweets); err != nil {
 		http.Error(pv.response, err.Error(), http.StatusInternalServerError)
 	}
+
+	// if err := ItemListTemplate.Execute(pv.response, tweets); err != nil {
+	// 	http.Error(pv.response, err.Error(), http.StatusInternalServerError)
+	// }
 }
 
 func (pv *PageView) DisplayError(err error, code int) {
