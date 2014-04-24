ec9b285c5c3a80b6572ffda060a525cb42948a6c
Casey Artner <Casey@Artner.co>
2014-04-23 22:48:26 -0700

    fix redirect code and refactor

--- tweetsaver/view_page.go
+++ tweetsaver/view_page.go
@@ -12,41 +12,41 @@ func init() {
 }
 
 type PageView struct {
-	response http.ResponseWriter
-	req      *http.Request
+	rw  http.ResponseWriter
+	req *http.Request
 }
 
 func NewPageView(w http.ResponseWriter) *PageView {
-	return &PageView{response: w}
+	return &PageView{rw: w}
 }
 
 func NewPageViewReq(w http.ResponseWriter, r *http.Request) *PageView {
-	return &PageView{response: w, req: r}
+	return &PageView{rw: w, req: r}
 }
 
 func (pv *PageView) DisplayItem(t *Tweet) {
-	if err := blueprint.Execute(pv.response, "Item.html", t); err != nil {
-		http.Error(pv.response, err.Error(), http.StatusInternalServerError)
+	if err := blueprint.Execute(pv.rw, "Item.html", t); err != nil {
+		http.Error(pv.rw, err.Error(), http.StatusInternalServerError)
 	}
 }
 
 func (pv *PageView) DisplayAll(tweets []*Tweet) {
-	if err := blueprint.Execute(pv.response, "ItemList.html", tweets); err != nil {
-		http.Error(pv.response, err.Error(), http.StatusInternalServerError)
+	if err := blueprint.Execute(pv.rw, "ItemList.html", tweets); err != nil {
+		http.Error(pv.rw, err.Error(), http.StatusInternalServerError)
 	}
 }
 
 func (pv *PageView) DisplayAddItem() {
-	if err := blueprint.Execute(pv.response, "AddItem.html", nil); err != nil {
-		http.Error(pv.response, err.Error(), http.StatusInternalServerError)
+	if err := blueprint.Execute(pv.rw, "AddItem.html", nil); err != nil {
+		http.Error(pv.rw, err.Error(), http.StatusInternalServerError)
 	}
 }
 
 func (pv *PageView) DisplayItemAdded(id int) {
 	newURL := fmt.Sprintf("/tweets.html/%d", id)
-	http.Redirect(pv.response, pv.req, newURL, http.StatusTemporaryRedirect)
+	http.Redirect(pv.rw, pv.req, newURL, http.StatusSeeOther)
 }
 
 func (pv *PageView) DisplayError(err error, code int) {
-	http.Error(pv.response, err.Error(), code)
+	http.Error(pv.rw, err.Error(), code)
 }
